03ec3e778e35ae327c10333d1e762decd3cd99fe
Nguyễn Thái Dương <165565744+duongad2003@users.noreply.github.com>
2024-11-21 10:46:56 +0700

    TV1

---
+++ Qlybantv-main/Qlybantv-main/Ictshop/Areas/Admin/Controllers/HedieuhanhsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Areas.Admin.Controllers
+{
+    public class HedieuhanhsController : Controller
+    {
+        private Qlbanhang db = new Qlbanhang();
+
+        // GET: Admin/Hedieuhanhs
+        public ActionResult Index()
+        {
+            return View(db.LoaiTVs.ToList());
+        }
+
+        // GET: Admin/Hedieuhanhs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiTV loaitv = db.LoaiTVs.Find(id);
+            if (loaitv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaitv);
+        }
+
+        // GET: Admin/Hedieuhanhs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Hedieuhanhs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaLoai,TenLoai")] LoaiTV loaitv)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LoaiTVs.Add(loaitv);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(loaitv);
+        }
+
+        // GET: Admin/Hedieuhanhs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiTV loaitv = db.LoaiTVs.Find(id);
+            if (loaitv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaitv);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaLoai,TenLoai")] LoaiTV loaitv)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(loaitv).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(loaitv);
+        }
+
+        // GET: Admin/Hedieuhanhs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiTV loaitv = db.LoaiTVs.Find(id);
+            if (loaitv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaitv);
+        }
+
+        // POST: Admin/Hedieuhanhs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LoaiTV loaitv = db.LoaiTVs.Find(id);
+            db.LoaiTVs.Remove(loaitv);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Qlybantv-main/Qlybantv-main/Ictshop/Models/LoaiTV.cs
@@ -0,0 +1,29 @@
+﻿namespace Ictshop.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("LoaiTV")]
+    public partial class LoaiTV
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public LoaiTV()
+        {
+            Sanpham = new HashSet<Sanpham>();
+        }
+
+        [Key]
+        public int MaLoai { get; set; }
+
+        [StringLength(10)]
+        [Display(Name = "Tên hệ điều hành")]
+
+        public string TenLoai { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Sanpham> Sanpham { get; set; }
+    }
+}
